17187b5bac6fe6adc3eb5473b934ed6d8fd25fbf
JEISONNAUSA <jeison.nausaa@gmail.com>
2023-10-15 10:35:26 -0500

    TALLER AVANZADO JAVA

---
+++ taller1/infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/bancolombia/jpa/JPARepository.java
@@ -0,0 +1,9 @@
+package co.com.bancolombia.jpa;
+
+import co.com.bancolombia.jpa.entities.AnimalEntities;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
+
+public interface JPARepository
+        extends CrudRepository<AnimalEntities/* change for adapter model */, String>,
+        QueryByExampleExecutor<AnimalEntities/* change for adapter model */> {}
